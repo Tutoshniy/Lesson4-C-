e01a85a84e28a614948fd25bed154c4f1aeddff8
Gleb <gleb.gadbrat@mail.ru>
2022-03-22 15:13:59 +0300

    Задача 4 - Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] nums = new int [N];
+void Mass(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        nums[i] = new Random().Next(0,1001);
+    }
+}
+Mass(N);
+Console.WriteLine("Произведения пар чисел: ");
+for (int i = 0; i < nums.Length/2; i++)
+{
+    Console.WriteLine(nums[i] * nums[nums.Length-1-i]);
+}
