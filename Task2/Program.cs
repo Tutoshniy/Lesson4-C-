59c7f4615cd64108b25381165f6ea1cd6cb573de
Gleb <gleb.gadbrat@mail.ru>
2022-03-22 13:03:54 +0300

    Задача 2 - В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Я использовал диапазон от 0 до 1000, потому что был очень большой разброс чисел. Если нужно использовать абсолютный рандом для int, 
+// То можно использовать вместо new Random().Next(0, 1000) - new Random().Next()
+Console.Clear();
+int numbers = 0;
+int [] nums = new int [123];
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = new Random().Next(0, 1000);
+    if (nums[i] >= 10)
+    {
+        if (nums[i] <= 99)
+        {
+            numbers++;   
+        }
+    }
+}
+Console.WriteLine();
+Console.Write("Количество чисел в диапазоне от 10 (включительно) до 99 (включительно): ");
+Console.WriteLine(numbers);
