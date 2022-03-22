f676f41b68dac3e1cf8cdcbf73f11a191e45a8e0
Gleb <gleb.gadbrat@mail.ru>
2022-03-22 13:32:54 +0300

    Задача 3 - Найти сумму чисел одномерного массива стоящих на нечетной позиции

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int sum = 0;
+Console.WriteLine("Введите количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] nums = new int[N];
+for (int i = 0; i < N; i++)
+{
+    nums[i] = new Random().Next(0, 1000);
+    if (i % 2 == 0)
+    {
+        sum += nums[i];
+    }
+}
+Console.Write("Сумма чисел на нечётных позициях равна ");
+Console.WriteLine(sum);
